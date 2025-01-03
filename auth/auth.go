955c9368e41fd5b6237d1900a1b45e97b856aaf0
Jack Anderson <jack.dev@alacriware.com>
2025-01-03 15:59:21 -0500

    authentication middleware; pseudocode added

--- auth/auth.go
+++ auth/auth.go
@@ -1,11 +1,26 @@
 package auth
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	// "fmt"
+
 	"github.com/gin-gonic/gin"
+	// "github.com/windingtheropes/fs.alacriware/based"
 )
 
+const TOKEN_LENGTH  = 64
+
+func NewToken(uid int, expiry int, max_uses int) string {
+	randomBytes := make([]byte, TOKEN_LENGTH)
+    _, err := rand.Read(randomBytes)
+    if err != nil {
+        panic(err)
+    }
+	return base64.URLEncoding.EncodeToString(randomBytes)[:TOKEN_LENGTH]
+}
 func Auth() gin.HandlerFunc {
-	return func(c *gin.Context) {
+	return func(c *gin.Context) {		
 		// if permitted
 		c.Next()
 		// else
@@ -14,3 +29,4 @@ func Auth() gin.HandlerFunc {
 
 	}
 }
+
