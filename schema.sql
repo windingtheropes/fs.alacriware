6eec4f42df1f972a90dd7fb14ad2fa1e41cfce8f
Jack Anderson <jack.dev@alacriware.com>
2025-01-04 12:13:04 -0500

    permissions apply only to groups, update schema with foreign keys for safety

--- schema.sql
+++ schema.sql
@@ -1,40 +1,47 @@
 CREATE TABLE usr (
-    id         INT AUTO_INCREMENT NOT NULL,
-    usr_name       VARCHAR(64),
-    PRIMARY KEY (`id`)
+    id          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    usr_name    VARCHAR(64)
 );
+INSERT INTO usr (usr_name) VALUES ("Default");
 
 CREATE TABLE grp (
-    id         INT AUTO_INCREMENT NOT NULL,
-    grp_name       VARCHAR(64),
-    PRIMARY KEY (`id`)
+    id          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    grp_name    VARCHAR(64)
 );
+INSERT INTO grp (grp_name) VALUES ("Default");
 
 CREATE TABLE token (
-    id      VARCHAR(64) NOT NULL,
+    id      VARCHAR(64) NOT NULL PRIMARY KEY,
     user_id INT NOT NULL,
     expiry  INT NOT NULL,
     max   INT NOT NULL,
     used    INT NOT NULL,
-    PRIMARY KEY (`id`)
+    FOREIGN KEY (user_id) REFERENCES usr(id)
 );
 
 CREATE TABLE permissions (
-    id      INT AUTO_INCREMENT NOT NULL,
+    id      INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     resource_path    VARCHAR(256) NOT NULL,
     group_id         INT NOT NULL,
-    user_id         INT NOT NULL,
     allowed    BOOLEAN NOT NULL,
     apply_recursive BOOLEAN NOT NULL,
-    PRIMARY KEY (`id`)
+    FOREIGN KEY (group_id) REFERENCES grp(id)
 );
 
 CREATE TABLE requests (
-    id      INT AUTO_INCREMENT NOT NULL,
+    id      INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     ip      VARCHAR(12) NOT NULL,
     access_time     BIGINT NOT NULL,
     resource_path    VARCHAR(256) NOT NULL,
     token   VARCHAR(64) NOT NULL, 
-    code INT NOT NULL,
-    PRIMARY KEY (`id`)
+    code INT NOT NULL
 );
+
+CREATE TABLE membership (
+    id  INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    user_id INT NOT NULL, 
+    group_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES usr(id),
+    FOREIGN KEY (group_id) REFERENCES grp(id)
+);
+INSERT INTO membership (user_id, group_id) VALUES (1,1);
