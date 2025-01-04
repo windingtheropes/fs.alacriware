2e341bea10c36ce8561e5db985eaa9b1004e7568
Jack Anderson <jack.dev@alacriware.com>
2025-01-04 11:02:33 -0500

    credentials struct and getcredentials from token for further processing based on userid

--- auth/auth.go
+++ auth/auth.go
@@ -3,12 +3,20 @@ package auth
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"fmt"
+	"time"
+
+	// "fmt"
+
 	// "fmt"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/windingtheropes/fs.alacriware/based"
+	"github.com/windingtheropes/fs.alacriware/based"
 )
-
+type Credentials struct {
+	UID int
+	Token string
+}
 const TOKEN_LENGTH  = 64
 
 func NewToken(uid int, expiry int, max_uses int) string {
@@ -19,10 +27,43 @@ func NewToken(uid int, expiry int, max_uses int) string {
     }
 	return base64.URLEncoding.EncodeToString(randomBytes)[:TOKEN_LENGTH]
 }
+func getCredentials(tQuery string) Credentials {
+		res, err := based.DB.FindToken(tQuery)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if len(res) < 1 {
+			// Token doesn't exist
+			return Credentials{
+				UID: 1,
+			}
+		} else {
+			// Token exists, make sure it is valid before authing as the user contained
+			token := res[0]
+			if(token.Used > token.Max) {
+				// Token is invalid due to max uses
+				return Credentials{
+					UID: 1,
+				}
+			}
+			if(time.Now().UnixMilli() > token.Expiry) {
+				// Token is invalid due to ttl
+				return Credentials{
+					UID: 1,
+				}
+			}
+			// The token is valid, so the user is acceptable
+			return Credentials{
+				UID: token.User_ID,
+			}
+		}
+}
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {		
 		// if permitted
 		c.Next()
+		credentials := getCredentials(c.Query("t"))
+		fmt.Println(credentials)
 		// else
 		// c.Status(403)
 		// c.Abort()
