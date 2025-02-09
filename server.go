8c02425361d8dba191ca249376cfc1e654d768d4
Jack Anderson <jack@alacriware.com>
2025-02-09 12:34:31 -0500

    fix query script

--- server.go
+++ server.go
@@ -52,38 +52,31 @@ func get_dir_list(path string) (string, error) {
 		fmt.Println(path + " is not a directory.")
 		return "", err
 	}
-	if err != nil {
-		fmt.Println("Error while gen dir path " + path + ":" + err.Error())
-		return "", err
-	}
 
 	files, err := os.ReadDir(path)
 	if err != nil {
 		fmt.Println("Error while gen dir path " + path + ":" + err.Error())
 		return "", err
 	}
-	var script string = `
-		<script>
-			const get_query = (url) => {
+	var html_list string = fmt.Sprintf("<h1>%v</h1>",safe_path(path))
+	if len(files) > 250 {
+		return fmt.Sprintf("Too many files (%v)", len(files)), nil
+	}
+	for i := 0; i < len(files); i++ {
+		rel_path := safe_path(filepath.Join(path, files[i].Name()))
+		html_list = fmt.Sprintf(`%v<br>%v`, html_list, fmt.Sprintf(`<a href="#" onclick="link_click('%v')")'>%v</a>`,rel_path,rel_path))
+	}
+	var query_script string = 
+			`const get_query = (url) => {
 				let parts = url.split('?')
 				const front = parts[0]
 				parts.shift()        
 				return parts.join('?')
 			}
 			const link_click = (url) => {
-				window.location.replace(url+'?'+get_query(window.location.href)})
-			}
-		</script>
-	`
-	var html string = fmt.Sprintf("%v,<h1>%v</h1>",script, safe_path(path))
-	if len(files) > 250 {
-		return fmt.Sprintf("Too many files (%v)", len(files)), nil
-	}
-	for i := 0; i < len(files); i++ {
-		rel_path := safe_path(filepath.Join(path, files[i].Name()))
-		html = html + "<br>" + fmt.Sprintf("<a href='#' onclick='link_click(%v)'>%v</>",rel_path,rel_path)
-	}
-	return html, nil
+				window.location.replace( url+'?'+get_query(window.location.href) )
+			}`
+	return fmt.Sprintf(`<html><head><script>%v</script></head><body>%v</body></html>`, query_script, html_list), nil
 }
 func main() {
 	file_server()
